c8a05bfa95c9199d931f316b1b72b64d8dff0673
AlyamanAlhijazi <alyamanmohamad22@gmail.com>
2025-03-10 13:02:57 +0100

    cocktails en user models aangemaakt

---
+++ models/Cocktail.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const cocktailSchema = new mongoose.Schema({
+  name: String,
+  ingredients: [String],
+  category: String,
+  alcohol: Boolean,
+  rating: { type: Number, default: 0 },
+  reviews: [{ user: String, rating: Number, comment: String }]
+});
+
+module.exports = mongoose.model('Cocktail', cocktailSchema);
---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connectie
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Verbonden met MongoDB'))
+  .catch(err => console.error('Database connectie mislukt:', err));
+
+app.listen(PORT, () => console.log(`Server draait op http://localhost:${PORT}`));
