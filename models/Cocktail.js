b4d1abf58d235b646261380a14fabf35e329429b
AlyamanAlhijazi <alyamanmohamad22@gmail.com>
2025-03-16 14:19:16 +0100

    mijn wijzigingen voor merge met main-dev

--- models/Cocktail.js
+++ models/Cocktail.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-// Definieer het schema
-const cocktailSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  ingredients: { type: [String], required: true },
-  category: { type: String },
-  alcohol: { type: Boolean },
-  rating: { type: Number, default: 0 },
-  reviews: [
-    {
-      user: { type: String },
-      rating: { type: Number },
-      comment: { type: String },
-    },
-  ],
-});
+// // Definieer het schema
+// const cocktailSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   ingredients: { type: [String], required: true },
+//   category: { type: String },
+//   alcohol: { type: Boolean },
+//   rating: { type: Number, default: 0 },
+//   reviews: [
+//     {
+//       user: { type: String },
+//       rating: { type: Number },
+//       comment: { type: String },
+//     },
+//   ],
+// });
 
-// Maak en exporteer het Cocktail model
-const Cocktail = mongoose.model("Cocktail", cocktailSchema);
-export default Cocktail;
+// // Maak en exporteer het Cocktail model
+// const Cocktail = mongoose.model("Cocktail", cocktailSchema);
+// export default Cocktail;
 
--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-// Definieer het schema voor een gebruiker
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
+// // Definieer het schema voor een gebruiker
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true },
+//   email: { type: String, required: true },
+//   password: { type: String, required: true },
+// });
 
-// Maak en exporteer het User model
-const User = mongoose.model("User", userSchema);
-export default User;
+// // Maak en exporteer het User model
+// const User = mongoose.model("User", userSchema);
+// export default User;
