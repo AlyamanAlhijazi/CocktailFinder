9564770f3b748701c7a5699ff8bfd2ece388e358
AlyamanAlhijazi <alyamanmohamad22@gmail.com>
2025-03-16 15:30:58 +0100

    idk

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ async function popularCocktails(req, res) {
 // Multer storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/uploads/'); ]
+        cb(null, 'public/uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
